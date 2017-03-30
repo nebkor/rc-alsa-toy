2acd1a9ad53731265f8dbdf1e500aa6401911d5c
Joe Ardent <code@ardent.nebcorp.com>
2017-03-30 14:11:43 -0400

    Sends raw PCM data to stdout.
    Stolen shamelessly from https://www.linuxjournal.com/article/6735?page=0,2

--- alsa-capture.c
+++ alsa-capture.c
@@ -1,90 +1,108 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+
+  This example reads from the default PCM device
+  and writes to standard output for 5 seconds of data.
+
+*/
+
+/* Use the newer ALSA API */
+#define ALSA_PCM_NEW_HW_PARAMS_API
+
 #include <alsa/asoundlib.h>
 
-int main (int argc, char *argv[])
-{
-  int i;
-  int err;
-  short buf[128];
-  snd_pcm_t *capture_handle;
-  snd_pcm_hw_params_t *hw_params;
-  unsigned int samprate = 44100;
+int main() {
+  long loops;
+  int rc;
+  int size;
+  snd_pcm_t *handle;
+  snd_pcm_hw_params_t *params;
+  unsigned int val;
+  int dir;
+  snd_pcm_uframes_t frames;
+  char *buffer;
 
-  if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-    fprintf (stderr, "cannot open audio device %s (%s)\n",
-             argv[1],
-             snd_strerror (err));
-    exit (1);
-  } else {
-    fprintf(stderr, "opened %s\n", argv[1]);
+  /* Open PCM device for recording (capture). */
+  rc = snd_pcm_open(&handle, "default",
+                    SND_PCM_STREAM_CAPTURE, 0);
+  if (rc < 0) {
+    fprintf(stderr,
+            "unable to open pcm device: %s\n",
+            snd_strerror(rc));
+    exit(1);
   }
 
-  if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
-    fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
-             snd_strerror (err));
-    exit (1);
+  /* Allocate a hardware parameters object. */
+  snd_pcm_hw_params_alloca(&params);
+
+  /* Fill it in with default values. */
+  snd_pcm_hw_params_any(handle, params);
+
+  /* Set the desired hardware parameters. */
+
+  /* Interleaved mode */
+  snd_pcm_hw_params_set_access(handle, params,
+                               SND_PCM_ACCESS_RW_INTERLEAVED);
+
+  /* Signed 16-bit little-endian format */
+  snd_pcm_hw_params_set_format(handle, params,
+                               SND_PCM_FORMAT_S16_LE);
+
+  /* Two channels (stereo) */
+  snd_pcm_hw_params_set_channels(handle, params, 2);
+
+  /* 44100 bits/second sampling rate (CD quality) */
+  val = 44100;
+  snd_pcm_hw_params_set_rate_near(handle, params,
+                                  &val, &dir);
+
+  /* Set period size to 32 frames. */
+  frames = 32;
+  snd_pcm_hw_params_set_period_size_near(handle,
+                                         params, &frames, &dir);
+
+  /* Write the parameters to the driver */
+  rc = snd_pcm_hw_params(handle, params);
+  if (rc < 0) {
+    fprintf(stderr,
+            "unable to set hw parameters: %s\n",
+            snd_strerror(rc));
+    exit(1);
   }
 
-  if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0) {
-    fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
+  /* Use a buffer large enough to hold one period */
+  snd_pcm_hw_params_get_period_size(params,
+                                    &frames, &dir);
+  size = frames * 4; /* 2 bytes/sample, 2 channels */
+  buffer = (char *) malloc(size);
 
-  if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-    fprintf (stderr, "cannot set access type (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
+  /* We want to loop for 5 seconds */
+  snd_pcm_hw_params_get_period_time(params,
+                                    &val, &dir);
+  loops = 5000000 / val;
 
-  if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
-    fprintf (stderr, "cannot set sample format (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &samprate, 0)) < 0) {
-    fprintf (stderr, "cannot set sample rate (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2)) < 0) {
-    fprintf (stderr, "cannot set channel count (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0) {
-    fprintf (stderr, "cannot set parameters (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
-
-  snd_pcm_hw_params_free (hw_params);
-
-  if ((err = snd_pcm_prepare (capture_handle)) < 0) {
-    fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
-             snd_strerror (err));
-    exit (1);
-  }
-
-
-  FILE *write_ptr;
-
-  write_ptr = fopen("test.pcm","wb");  // w for write, b for binary
-
-  for (i = 0; i < 10000; ++i) {
-    if ((err = snd_pcm_readi (capture_handle, buf, 128)) != 128) {
-      fprintf (stderr, "read from audio interface failed (%s)\n",
-               snd_strerror (err));
-      exit (1);
+  while (loops > 0) {
+    loops--;
+    rc = snd_pcm_readi(handle, buffer, frames);
+    if (rc == -EPIPE) {
+      /* EPIPE means overrun */
+      fprintf(stderr, "overrun occurred\n");
+      snd_pcm_prepare(handle);
+    } else if (rc < 0) {
+      fprintf(stderr,
+              "error from read: %s\n",
+              snd_strerror(rc));
+    } else if (rc != (int)frames) {
+      fprintf(stderr, "short read, read %d frames\n", rc);
     }
-
-    fwrite(buf, 128, 1, write_ptr);
+    rc = write(1, buffer, size);
+    if (rc != size)
+      fprintf(stderr,
+              "short write: wrote %d bytes\n", rc);
   }
 
-  snd_pcm_close (capture_handle);
-  exit (0);
+  snd_pcm_drain(handle);
+  snd_pcm_close(handle);
+  free(buffer);
+
+  return 0;
 }
